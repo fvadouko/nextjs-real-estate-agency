dd7a8f201960bed0d788f0d5aff00acdb078df15
ADOUKO Franck Venance <fvadouko@live.fr>
2022-02-05 19:29:43 +0000

    implementation of pagination in the bottom of the available properties.

--- pages/properties.js
+++ pages/properties.js
@@ -1,31 +1,57 @@
 import { MDBContainer } from "mdbreact";
-import Head from "next/head";
-import Image from "next/image";
+
 import api from "../auth/axios";
-import Caroussel from "../components/caroussel";
-import Features from "../components/features";
 import Layout from "../components/layout";
 import ListProperties from "../components/listProperties";
-import PropertiesSection from "../components/propertiesSection";
-import PropertiesVip from "../components/propertiesVip";
-import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import ReactPaginate from "react-paginate";
 
-export default function Properties({ properties }) {
+export default function Properties({ properties, currentPage, totalPages }) {
+  const router = useRouter();
+  const paginateHandler = (page) => {
+    const currentPath = router.pathname;
+    let currentQuery = { ...router.query };
+    currentQuery.page = page.selected + 1;
+    router
+      .push({
+        pathname: currentPath,
+        query: currentQuery,
+      })
+      .then(() => window.scrollTo(0, 0));
+  };
   return (
     <Layout>
       <MDBContainer>
         <ListProperties properties={properties} />
       </MDBContainer>
+      <div className="paginateCenter">
+        <ReactPaginate
+          onPageChange={paginateHandler}
+          initialPage={currentPage - 1}
+          pageCount={totalPages}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          activeClassName="activated"
+          breakLabel="..."
+          pageClassName="paginate"
+          containerClassName="custom-paginate"
+        />
+      </div>
     </Layout>
   );
 }
 
-export const getServerSideProps = async () => {
-  const { data } = await api.get("/api/properties");
+export const getServerSideProps = async ({ query }) => {
+  const page = query.page || 1;
+  const { data } = await api.get(`/api/properties?page=${page}`);
   const properties = data.data;
+  const currentPage = data.currentPage;
+  const totalPages = data.totalPages;
   return {
     props: {
       properties,
+      currentPage,
+      totalPages,
     },
   };
 };
