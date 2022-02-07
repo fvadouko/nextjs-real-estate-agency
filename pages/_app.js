7a4dc90caf77db1c7c7390e842a5d3574f389bc9
ADOUKO Franck Venance <fvadouko@live.fr>
2022-02-07 16:50:33 +0000

    Copy of files for authentications

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
+import { AuthProvider } from "../auth/context";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
